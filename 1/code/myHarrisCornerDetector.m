eb59b8d10ca7e99c822e737e2f1633a52d460eb5
HUe1998 <himanshu.1998me@gmail.com>
2020-10-18 16:57:57 +0530

    Complete Parameter tuning with report

--- 1/code/myHarrisCornerDetector.m
+++ 1/code/myHarrisCornerDetector.m
@@ -1,21 +1,14 @@
 function [Ix,Iy,Ie1,Ie2,Icornr]=myHarrisCornerDetector(img,sg1,sg2,k)
-%     hfWnd = 2;      %half Window Size of patch
     Ismth = imgaussfilt(img, sg1);
     [nr, nc] = size(img);
-%     gFilter = fspecial('gaussian', 2*hf+1, sg2);
 
     % Image Derivative Calculation
-%     Ix = conv2(Ismth, sobelX, 'same');
-%     Iy = conv2(Ismth, sobelY, 'same');
     [Ix, Iy] = imgradientxy(Ismth);
     Ix2 = Ix .^ 2;
     Iy2 = Iy .^ 2;
     Ixy = Ix .* Iy;
     
-    % Weighted gaussian sum of products around patch size 2*hfWnd + 1;
-%     Wx2 = conv2(Ix2, gFilter, 'same');
-%     Wy2 = conv2(Iy2, gFilter, 'same');
-%     Wxy = conv2(Ixy, gFilter, 'same');
+    % Weighted gaussian sum;
     Wx2 = imgaussfilt(Ix2, sg2);
     Wy2 = imgaussfilt(Iy2, sg2);
     Wxy = imgaussfilt(Ixy, sg2);
--- 1/code/myMainScript.m
+++ 1/code/myMainScript.m
@@ -2,37 +2,34 @@
 
 tic;
 %% Your code here
-% sobelX = [1 0 -1; 2 0 -2; 1 0 -1];
-% sobelY = [1 2 1; 0 0 0; -1 -2 -1];
 img = rescale(load('../data/boat.mat').imageOrig);
-[Ix,Iy,Ie1,Ie2,Icornr] = myHarrisCornerDetector(img, 1.35, 0.5, 0.06);
+[Ix,Iy,Ie1,Ie2,Icornr] = myHarrisCornerDetector(img, 2, 0.45, 0.12);
 
 %Output Images
-% figure('Name', 'Derivative Images');
-% subplot(1,2,1), imshow(Ix);
-% axis equal tight on;
-% title('Derivative along X-axis');
-% colorbar;
-% subplot(1,2,2), imshow(Iy);
-% axis equal tight on;
-% title('Derivative along Y-axis');
-% colorbar;
+figure('Name', 'Derivative Images');
+subplot(1,2,1), imshow(Ix);
+axis equal tight on;
+title('Derivative along X-axis');
+colorbar;
+subplot(1,2,2), imshow(Iy);
+axis equal tight on;
+title('Derivative along Y-axis');
+colorbar;
 
-% figure('Name', 'Eigenvalue Images');
-% subplot(1,2,1), imshow(Ie2);
-% axis equal tight on;
-% title('Principal Eigenvalue');
-% colorbar;
-% subplot(1,2,2), imshow(Ie1);
-% axis equal tight on;
-% title('Another Eigenvalue');
-% colorbar;
+figure('Name', 'Eigenvalue Images');
+subplot(1,2,1), imshow(Ie2);
+axis equal tight on;
+title('Principal Eigenvalue');
+colorbar;
+subplot(1,2,2), imshow(Ie1);
+axis equal tight on;
+title('Another Eigenvalue');
+colorbar;
 
-% figure('Name', 'Harris Corner Image');
-% imshow(Icornr);
-
-% figure, imshow(img);
-figure, imshow(Ix);
-figure, imshow(Iy);
+figure('Name', 'Harris Corner Image');
+imshow(Icornr);
+axis equal tight on;
+title('Harris Corner Image');
+colorbar;
 
 toc;
